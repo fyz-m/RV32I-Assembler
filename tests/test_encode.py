e346e0ffe4848bfb9b0ba97ee728c9055d2e5994
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-03-28 11:08:08 +0400

    Improved test structure

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,28 +1,32 @@
 from src.encode import Instruction
+import pytest
+
+@pytest.mark.parametrize("input_inst, expected_mnemonic",[
+        ("add s3, s1, s2",  "add"),   
+        ("addi s3, s1, 10", "addi"),  
+        ("sw s3, 12(s2)", "sw"), 
+        ("beq s3, s1, label", "beq"),  
+        ("lui s3, s1, 0xABCDEF)", "lui"),   
+        ("jal ra, label", "jal"), 
 
 
-def test_getType():
-  instruction = Instruction("add s3, s1, s2")
-  assert instruction.Type == "R-type"
-  del instruction
-  
-  instruction = Instruction("addi s3, s1, 10") 
-  assert instruction.Type == "I-type"
-  del instruction
-  
-  instruction = Instruction("sw s3, 12(s2)") 
-  assert instruction.Type == "S-type"
-  del instruction
-  
-  instruction = Instruction("beq s3, s1, label") 
-  assert instruction.Type == "B-type"
-  del instruction
-  
-  instruction = Instruction("lui s3, s1, 0xABCDEF") 
-  assert instruction.Type == "U-type"
-  del instruction
-  
-  instruction = Instruction("jal ra, label") 
-  assert instruction.Type == "J-type"
-  del instruction
-  
+])
+
+def test_mnemonic(input_inst, expected_mnemonic):
+  instruction = Instruction(input_inst)
+  assert instruction.Mnemonic == expected_mnemonic
+
+
+@pytest.mark.parametrize("input_inst, expected_type", [
+        ("add s3, s1, s2",  "R-type"),   
+        ("addi s3, s1, 10", "I-type"),  
+        ("sw s3, 12(s2)", "S-type"), 
+        ("beq s3, s1, label", "B-type"),  
+        ("lui s3, s1, 0xABCDEF)", "U-type"),   
+        ("jal ra, label", "J-type"), 
+    ])
+
+def test_type(input_inst, expected_type):
+  instruction = Instruction(input_inst)
+  assert instruction.Type == expected_type
+
