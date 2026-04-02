46dfb4f6479bc6374280d786c8d89e7307ff0332
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-02 10:29:54 +0400

    Add function for R-type encoding

--- src/encode.py
+++ src/encode.py
@@ -5,38 +5,76 @@ def encode(instruction):
   Takes Instruction object as input (contains field values like opcode and registers)
   Returns string of field values encoded according to its instruction type 
   '''
-
+  fields = [
+    instruction.op, instruction.rd, instruction.rs1, instruction.rs2, instruction.imm, instruction.func3, instruction.func7
+]
   match instruction.Type:
     case "R-type":
-      encode_R_type(instruction)
+      encode_R_type(fields)
     case "I-type":
-      encode_I_type(instruction)
+      encode_I_type(fields)
     case "S-type":
-      encode_S_type(instruction)
+      encode_S_type(fields)
     case "B-type":
-      encode_B_type(instruction)
+      encode_B_type(fields)
     case "U-type":
-      encode_U_type(instruction)
+      encode_U_type(fields)
     case "B-type":
-      encode_B_type(instruction)
+      encode_B_type(fields)
+
+
+'''
+  Encoding the values into 32-bit machine code is done by:
+   - shifting the value to its bit postion, padding least significant bits with 0
+   - OR-ing all the values, resulting in concatenation
+
+   e.g:
+   bin1 = 0110
+   bin2 = 1111
+
+   bin2 << 4 = bin2_0000
+   bin1 | bin2 = 
+                0000_0110
+                |||| ||||   bit-wise OR
+                1111_0000
+                ---------
+                1111_0110
+'''
+
+def encode_R_type(op, rd, rs1, rs2, funct3, funct7):
+  '''
+  R-type instruction are encoded as the following:
+  
+  bit[6:0] = opcode
+  bit[11:7] = destination register (rd)
+  bit[14:12] = function field 3
+  bit[19:15] = source register 1 (rs1)
+  bit[24:20] = source register 2 (rs2)
+  bit[31:25] = function field 7
+
+  '''
+  # Shift the value to its starting bit postion, pad with zeroes 
+  rd = rd << 7   # rd = rd_000_0000
+  funct3 = funct3 << 12
+  rs1 = rs1 << 15
+  rs2 = rs2 << 20
+  funct7 = funct7 << 25
+  
+  return op | rd | funct3 | rs1 | rs2 | funct7
 
 
 
-
-def encode_R_type(instruction):
+def encode_I_type():
   ...
 
-def encode_I_type(instruction):
-  ...
-
-def encode_S_type(instruction):
+def encode_S_type():
   ... 
 
-def encode_B_type(instruction):
+def encode_B_type():
   ... 
 
-def encode_U_type(instruction):
+def encode_U_type():
   ... 
 
-def encode_J_type(instruction):
+def encode_J_type():
   ... 
