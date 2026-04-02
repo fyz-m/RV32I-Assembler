cc950056b42442864c2dcab6ddcfa52263b2ddcf
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-02 10:40:02 +0400

    Add tests for I-type encoding

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -7,8 +7,17 @@ def test_encode_R_type():
 
 
 def test_encode_I_type():
-  ...
-
+  # addi s0, s1, 12
+  assert src.encode.encode_I_type(op=19, rd=8, rs1=9, funct3=0, imm=12) == 0x00C48413
+  # addi s2, t1, -14
+  assert src.encode.encode_I_type(op=19, rd=18, rs1=6, funct3=0, imm=-14) == 0xFF230913
+  # lw t2, -6(s3)
+  assert src.encode.encode_I_type(op=3, rd=7, rs1=19, funct3=3, imm=-6) == 0xFFA9A383
+  # slli s2, s7, 5
+  assert src.encode.encode_I_type(op=19, rd=18, rs1=23, funct3=1, imm=5) == 0x005B9913
+  # srai t1, t2, 29  
+  assert src.encode.encode_I_type(op=19, rd=6, rs1=7, funct3=5, imm=29) == 0x41D3D313
+ 
 def test_encode_S_type():
   ...
 
