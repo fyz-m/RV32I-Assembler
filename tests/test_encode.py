530d7fd7578c3232c791da8c3297b299a8d55947
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-02 12:46:23 +0400

    Fix I-type test

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -1,6 +1,7 @@
 import src.encode
 import pytest
 
+
 def test_encode_R_type():
   assert src.encode.encode_R_type(op=51, rd=18, rs1=19, rs2=20, funct3=0, funct7=0) == 0x01498933
   assert src.encode.encode_R_type(op=51, rd=5, rs1=6, rs2=7, funct3=0, funct7=32) == 0x407302B3
@@ -12,7 +13,7 @@ def test_encode_I_type():
   # addi s2, t1, -14
   assert src.encode.encode_I_type(op=19, rd=18, rs1=6, funct3=0, imm=-14) == 0xFF230913
   # lw t2, -6(s3)
-  assert src.encode.encode_I_type(op=3, rd=7, rs1=19, funct3=3, imm=-6) == 0xFFA9A383
+  assert src.encode.encode_I_type(op=3, rd=7, rs1=19, funct3=2, imm=-6) == 0xFFA9A383
   # slli s2, s7, 5
   assert src.encode.encode_I_type(op=19, rd=18, rs1=23, funct3=1, imm=5) == 0x005B9913
   # srai t1, t2, 29  
