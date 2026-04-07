66ebc82a3555bb7bcac61835cb36d2eac0ccf121
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-07 09:49:14 +0400

    Add CLI parsing

---
+++ main.py
@@ -0,0 +1,25 @@
+import argparse
+from src.parse import assemble
+
+def main():
+
+  file, bin = get_arguments()
+  assemble(file)
+
+  
+def get_arguments():
+  ...
+  #Get input file to be converted and CLI arguments to output in binary or hex
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("-f", "--file", help="Assembly file to be converted into machine code")
+  parser.add_argument("-b", "--binary", help="Output assembled file in binary (default: hexadecimal)",  action="store_true")
+
+  args = parser.parse_args()
+  
+  return args.file, args.binary
+
+
+
+if __name__ == "__main__":
+  main()
